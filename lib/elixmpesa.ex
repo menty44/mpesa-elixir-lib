ac3ac6c57624d628b579baf6242243afce0592c3
menty44 <menty44@gmail.com>
2019-10-11 09:26:27 +0300

    first commit

---
+++ lib/elixmpesa.ex
@@ -0,0 +1,18 @@
+defmodule Elixmpesa do
+  @moduledoc """
+  Documentation for Elixmpesa.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Elixmpesa.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/elixmpesa_test.exs
@@ -0,0 +1,8 @@
+defmodule ElixmpesaTest do
+  use ExUnit.Case
+  doctest Elixmpesa
+
+  test "greets the world" do
+    assert Elixmpesa.hello() == :world
+  end
+end
