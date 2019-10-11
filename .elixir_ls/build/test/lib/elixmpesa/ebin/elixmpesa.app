6c77cddbfc252c3f68f1ae07ce9f4ae812f40cb2
menty44 <menty44@gmail.com>
2019-10-11 09:44:03 +0300

    added some info on project

---
+++ .elixir_ls/build/test/lib/elixmpesa/ebin/elixmpesa.app
@@ -0,0 +1,6 @@
+{application,elixmpesa,
+             [{applications,[kernel,stdlib,elixir,logger]},
+              {description,"Mpesa Lib using Elixir Language.\n"},
+              {modules,['Elixir.Elixmpesa']},
+              {registered,[]},
+              {vsn,"0.1.0"}]}.
--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule Elixmpesa.MixProject do
       version: "0.1.0",
       elixir: "~> 1.8",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      description: description(),
+      deps: deps(),
+      package: package(),
     ]
   end
 
@@ -23,6 +25,22 @@ defmodule Elixmpesa.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+    ]
+  end
+
+  def description do
+    """
+    Mpesa Lib using Elixir Language.
+    """
+  end
+
+  def package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["FREDRICK OLUOCH"],
+      email: ["menty44@gmail.com"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/menty44/mpesa-elixir-lib"}
     ]
   end
 end
